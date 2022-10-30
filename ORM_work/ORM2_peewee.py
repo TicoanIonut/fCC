ffe1cbe990776b4ca5e1a668b14facf5a80286c6
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-10-30 19:21:58 +0200

    30.10.2022

--- ORM_work/ORM2_peewee.py
+++ ORM_work/ORM2_peewee.py
@@ -1,4 +1,33 @@
+import pandas as pd
 from db_models_pwi import *
 
-reviews = Reviews.select().join(Genres).get()
-print(reviews.artist)
+
+# con = Reviews.select(Reviews.artist).filter(Reviews.score < 6)
+qry = Reviews.select(Reviews).filter(Reviews.score < 6)
+# print(qry)
+# print(con)
+
+# rrr = []
+#
+# for s in con:
+# 	s.artist = '111' + s.artist
+# 	rrr.append({'artist': s.artist})
+# 	# print(s.artist)
+# df1 = pd.DataFrame(rrr)
+# print(df1.describe())
+
+
+lll = []
+for i in qry:
+	i.artist = '111' + i.artist
+	lll.append({'artist': i.artist,
+	            'title': i.title,
+	            'reviewid': i.reviewid,
+	            'score': i.score,
+	            # 'url': i.url
+	            })
+df = pd.DataFrame(lll)
+# print(df.describe)
+print(df)
+# rez = pd.concat([df1, df], axis=1)
+# print(rez)
