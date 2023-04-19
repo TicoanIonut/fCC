4b6acc1418c913fa282985dd5c1f08f3269f5d19
Ticoan Ionut <iouticoan@gmail.com>
2023-04-19 20:50:20 +0300

    update

--- rotating_proxy/proxy_rotation.py
+++ rotating_proxy/proxy_rotation.py
@@ -2,8 +2,8 @@ import requests
 
 with open('valid_proxy.txt', 'r') as f:
 	proxies = f.read().split('\n')
-	# print(len(proxies))
-	
+# print(len(proxies))
+
 sites_to_check = ['http://books.toscrape.com/',
                   'http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html',
                   'http://books.toscrape.com/catalogue/category/books/adult-fiction_29/index.html',
@@ -13,8 +13,8 @@ counter = 0
 for site in sites_to_check:
 	try:
 		print(f'using the proxy: {proxies[counter]}')
-		res = requests.get(site, proxies={ 'http': proxies[counter],
-		                                   'https:': proxies[counter]})
+		res = requests.get(site, proxies={'http': proxies[counter],
+		                                  'https:': proxies[counter]})
 		print(res.status_code)
 	except:
 		print('fail')
